21a0d0d4a09eb8d0716afa460e97752ec8649843
kd383 <dongkun_29@hotmail.com>
2017-02-18 21:02:25 -0500

    changed things

--- doc/demoLanRecovery.m
+++ doc/demoLanRecovery.m
@@ -13,8 +13,8 @@ Y = generate_data(X,opt_Y);
 % Setup SKI and FITC
 xg = apxGrid('create',X,true,ngrid);
 xu = linspace(-2,2,ngrid)';
-%cov = {@(varargin)covMaterniso(1,varargin{:})};
-cov = {@covSEiso};
+cov = {@(varargin)covMaterniso(1,varargin{:})};
+%cov = {@covSEiso};
 covg = {@apxGrid,cov,xg};
 covf = {@apxSparse,cov,xu};
 lik = {@likGauss};
@@ -27,20 +27,9 @@ opt_sur.npts = 200; opt_sur.ntrials = 1000; opt_sur.param = {{'cov','lik'},[2,1]
 opt_sur.bounds = log([5e-2,0.2,1e-2;2,2,0.5]); opt_sur.method = 'lanczos';
 opt_sur.cg_maxit = 500; opt_sur.cg_tol = 1e-5; opt_sur.replace_diag = 1; opt_sur.nZ = 30;
 
-tic;
-sur = build_surrogate(covg,X,opt_sur);
-time(1) = toc;
-
-% Sur + Lan + Diag_Corr
-opt1.cg_maxit = 500; opt1.cg_tol = 1e-3; opt1.replace_diag = 1; %opt1.ldB2_lan_hutch = sign(randn(N,10));
-opt1.ldB2_sur = sur;  %opt1.ldB2_lan_reorth = 0; %opt1.ldB2_lan_kmax = 100;
-inf1 = @(varargin)infGaussLik(varargin{:},opt1);
-
 % Lan + Diag_Corr
 opt2.cg_maxit = 500; opt2.cg_tol = 1e-3; opt2.replace_diag = 1;
-opt2.ldB2_lan = 1; opt2.ldB2_lan_hutch = sign(randn(N,30));
-opt2.ldB2_lan_reorth = 1; opt2.ldB2_lan_kmax = 100;
-inf2 = @(varargin)infGaussLik(varargin{:},opt2);
+opt2.ldB2_lan = 1; opt2.ldB2_lan_reorth = 1; opt2.ldB2_lan_kmax = 100;
 
 % Apx + Diag_Corr
 opt3.cg_maxit = 500; opt3.cg_tol = 1e-3; opt3.replace_diag = 1;
@@ -61,41 +50,70 @@ inf6 = @(varargin)infGaussLik(varargin{:},opt6);
 for nrun = 1:1
     hyp0 = struct('mean', [], 'cov', 0.9*hyp.cov,'lik', 0.9*hyp.lik);
     
-    % Sur + Lan + Diag_Corr
-    tic;
-    temp1 = minimize(hyp0,@gp,-100,inf1,means,covg,lik,X,Y);
-    hyp_sur(nrun,:) = exp([temp1.cov',temp1.lik]);
-    time(2) = time(2) + toc;
+    hyp_recover = cell(6,1);
+    NLK = cell(6,1);
     
-    % Lan + Diag_Corr
-    tic;
-    temp2 = minimize(hyp0,@gp,-100,inf2,means,covg,lik,X,Y);
-    hyp_lan(nrun,:) = exp([temp2.cov',temp2.lik]);
-    time(3) = time(3) + toc;
-   
+    for j = 1:5
+        tic;
+        sur = build_surrogate(covg,X,opt_sur);
+        time(1) = time(1) + toc;
+        % Sur + Lan + Diag_Corr
+        opt1.cg_maxit = 500; opt1.cg_tol = 1e-3; opt1.replace_diag = 1; 
+        opt1.ldB2_sur = sur;
+        inf1 = @(varargin)infGaussLik(varargin{:},opt1);
+        % Sur + Lan + Diag_Corr
+        tic;
+        temp1 = minimize(hyp0,@gp,-100,inf1,means,covg,lik,X,Y);
+        hyp_sur(j,:) = exp([temp1.cov',temp1.lik]);
+        time(2) = time(2) + toc;
+        [~,nlZ,dnlZ] = infGaussLik(temp1,means,cov,lik,X,Y,opt6);
+        NLK{1}(j,:) = [nlZ,dnlZ.cov',dnlZ.lik];
+    end
+    hyp_recover(1) ={hyp_sur};
+    
+    for j = 1:5
+        % Lan + Diag_Corr
+        opt2.ldB2_lan_hutch = sign(randn(N,30));
+        inf2 = @(varargin)infGaussLik(varargin{:},opt2);
+        tic;
+        temp2 = minimize(hyp0,@gp,-100,inf2,means,covg,lik,X,Y);
+        hyp_lan(j,:) = exp([temp2.cov',temp2.lik]);
+        time(3) = time(3) + toc;
+        [~,nlZ,dnlZ] = infGaussLik(temp2,means,cov,lik,X,Y,opt6);
+        NLK{2}(j,:) = [nlZ,dnlZ.cov',dnlZ.lik];
+    end
+    hyp_recover(2) ={hyp_lan};
+    
     tic;
     temp3 = minimize(hyp0,@gp,-100,inf3,means,covg,lik,X,Y);
-    hyp_gdc(nrun,:) = exp([temp3.cov',temp3.lik]);
+    hyp_recover(3) = {exp([temp3.cov',temp3.lik])};
     time(4) = time(4) + toc;
+    [~,nlZ,dnlZ] = infGaussLik(temp3,means,cov,lik,X,Y,opt6);
+    NLK{3} = {[nlZ,dnlZ.cov',dnlZ.lik]};
     
     tic;
     temp4 = minimize(hyp0,@gp,-100,inf4,means,covg,lik,X,Y);
-    hyp_grid(nrun,:) = exp([temp4.cov',temp4.lik]);
-    time(5) = time(4) + toc;
+    hyp_recover(4) = {exp([temp4.cov',temp4.lik])};
+    time(5) = time(5) + toc;
+    [~,nlZ,dnlZ] = infGaussLik(temp4,means,cov,lik,X,Y,opt6);
+    NLK{4} = {[nlZ,dnlZ.cov',dnlZ.lik]};
     
     tic;
     temp5 = minimize(hyp0,@gp,-100,inf5,means,covf,lik,X,Y);
-    hyp_gdc(nrun,:) = exp([temp5.cov',temp5.lik]);
+    hyp_recover(5) = {exp([temp5.cov',temp5.lik])};
     time(6) = time(6) + toc;
+    [~,nlZ,dnlZ] = infGaussLik(temp5,means,cov,lik,X,Y,opt6);
+    NLK{5} = {[nlZ,dnlZ.cov',dnlZ.lik]};
+    
     
     tic;
     temp6 = minimize(hyp0,@gp,-100,inf6,means,cov,lik,X,Y);
-    hyp_exact(nrun,:) = exp([temp6.cov',temp6.lik]);
+    hyp_recover(6) = {exp([temp6.cov',temp6.lik])};
     time(7) = time(7) + toc;
+    [~,nlZ,dnlZ] = infGaussLik(temp1,means,cov,lik,X,Y,opt6);
+    NLK{6} = {[nlZ,dnlZ.cov',dnlZ.lik]};
 end
 
-
-[post,nlZ,dnlZ] = infGaussLik(temp1,means,cov,lik,X,Y,opt6);[nlZ,dnlZ.cov',dnlZ.lik]
 
 %{
 result = exp([hyp.cov;hyp.lik]);
